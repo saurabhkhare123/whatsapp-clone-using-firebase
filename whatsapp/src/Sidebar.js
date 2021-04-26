7bc4b6569fa0846cada9b24efca73d6a69ccfb9d
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2021-04-26 10:38:26 +0530

    sidebar icons added

--- whatsapp/src/Sidebar.js
+++ whatsapp/src/Sidebar.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 import './Sidebar.css';
 function Sidebar() {
     return (
         <div className="sidebar">
 
-            <div className="sidebar_header"></div>
-            <div className="sidebar_search"></div>
-            <div className="sidebar_chats"></div>
-            
+            <div className="sidebar_header">
+                <Avatar />
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        < MoreVertIcon />
+                    </IconButton>
+
+                </div>
+            </div>
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                <SearchOutlined />
+                <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+            <div className="sidebar_chats">
+                <h1></h1>
+            </div>
+
         </div>
     )
 }
