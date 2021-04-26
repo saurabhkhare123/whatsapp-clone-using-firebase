b2635568c50b34cead070be331600074d8ec20a7
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2021-04-26 09:58:43 +0530

    basic files created

---
+++ whatsapp/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+     
+
+      < div className="app_body">
+        < Sidebar />
+        {/* Chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCWmzzPMxrO-gkJxGotEPxk4ugWEg4SunE",
+    authDomain: "whatsappclone-2604b.firebaseapp.com",
+    projectId: "whatsappclone-2604b",
+    storageBucket: "whatsappclone-2604b.appspot.com",
+    messagingSenderId: "365853881819",
+    appId: "1:365853881819:web:a35bedd50ecaa43bebe3fe",
+    measurementId: "G-YJ14V6H838"
+  };
