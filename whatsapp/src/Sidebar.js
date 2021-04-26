8f1c22b9143caff19bc8042d5cbb397aaf4d186c
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2021-04-26 22:12:51 +0530

    completed

--- whatsapp/src/Sidebar.js
+++ whatsapp/src/Sidebar.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import { SearchOutlined } from "@material-ui/icons";
+import { SearchOutlined, SignalCellular0Bar } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat"
 import './Sidebar.css';
+import db from './firebase';
+import { useStateValue } from './StateProvider';
 function Sidebar() {
+    const [rooms, setRooms] = useState([]);
+    const [{user}, dispatch] = useStateValue();
+
+    useEffect(() => {
+
+const unsubscribe= db.collection('rooms').onSnapshot(snapshot =>(
+    setRooms(snapshot.docs.map(doc => ({
+        id:doc.id,
+        data: doc.data(),
+    })))
+));
+return() =>{
+    unsubscribe();
+}
+    }, [])
     return (
         <div className="sidebar">
 
             <div className="sidebar_header">
-                <Avatar />
+                <Avatar src={user?.photoURL}/>
                 <div className="sidebar_headerRight">
                     <IconButton>
                         <DonutLargeIcon />
@@ -32,7 +50,10 @@ function Sidebar() {
                 </div>
             </div>
             <div className="sidebar_chats">
-                <h1></h1>
+                <SidebarChat addNewChat />
+               {rooms.map(room => (
+                 <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+               ))}
             </div>
 
         </div>
--- whatsapp/src/firebase.js
+++ whatsapp/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCWmzzPMxrO-gkJxGotEPxk4ugWEg4SunE",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:365853881819:web:a35bedd50ecaa43bebe3fe",
     measurementId: "G-YJ14V6H838"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth, provider};
+  export default db;
